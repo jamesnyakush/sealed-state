887e2258ea4c37e748fd5edef6fbe25ece271fc0
jamesnyakush <emvesttech@gmail.com>
2020-01-28 10:43:08 +0300

    added deps

--- app/src/main/java/com/jamesnyakush/recyclersealed/utils/BaseRepository.kt
+++ app/src/main/java/com/jamesnyakush/recyclersealed/utils/BaseRepository.kt
@@ -1,2 +1,33 @@
 package com.jamesnyakush.recyclersealed.utils
 
+import android.util.Log
+import retrofit2.Response
+import java.io.IOException
+
+open class BaseRepository {
+
+    /*suspend function used as a safe call to prevent UI Blockage when pulling info from API*/
+    suspend fun <T : Any> safeApiCall(call: suspend () -> Response<T>, errorMessage: String): T? {
+        val result: Result<T> = safeApiResult(call, errorMessage)
+        var data: T? = null
+        when (result) {
+            is Result.Success ->
+                data = result.data
+            is Result.Error -> {
+                Log.d("1.DataRepository", "$errorMessage & Exception - ${result.exception}")
+            }
+        }
+        return data
+    }
+
+    /*gets results from above*/
+    private suspend fun <T : Any> safeApiResult(
+        call: suspend () -> Response<T>,
+        errorMessage: String
+    ): Result<T> {
+        val response = call.invoke()
+        if (response.isSuccessful) return Result.Success(response.body()!!)
+
+        return Result.Error(IOException("Error Occurred during getting safe Api result, Custom ERROR - $errorMessage"))
+    }
+}
